e8e11a5f0facf9a874e08b30acdf842df21e7ff8
novellito <christian.trinidad.501@my.csun.edu>
2017-02-04 12:26:16 -0800

    trying to commit

--- app/src/main/java/com/ac/csun/team3/Listening.java
+++ app/src/main/java/com/ac/csun/team3/Listening.java
@@ -1,10 +1,20 @@
 package com.ac.csun.team3;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.os.Build;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Locale;
 import android.os.Handler;
 import android.os.SystemClock;
 import android.util.Log;
@@ -12,6 +22,8 @@ import android.view.View;
 import android.widget.ImageView;
 
 import java.util.Locale;
+
+import static android.R.attr.text;
 
 public class Listening extends Activity implements View.OnClickListener {
     private ImageView settingsButton;
@@ -21,7 +33,7 @@ public class Listening extends Activity implements View.OnClickListener {
     private Handler wait = new Handler();
 
 
-    private TextToSpeech tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+    private TextToSpeech tts= new TextToSpeech(this, new TextToSpeech.OnInitListener() {
         @Override
         public void onInit(int status) {
             if (status == TextToSpeech.SUCCESS) {
@@ -29,7 +41,7 @@ public class Listening extends Activity implements View.OnClickListener {
                 if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
                     Log.e("TTS", "This Language is not supported");
                 }
-                speak("Listening");
+                speak("Listening...");
 
             } else {
                 Log.e("TTS", "Initilization Failed!");
@@ -37,12 +49,44 @@ public class Listening extends Activity implements View.OnClickListener {
         }
     });
 
-    private void speak(String text){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
-        }else{
-            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        private void listen() {
+            Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+            i.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something");
+
+            try {
+                startActivityForResult(i, 100);
+            } catch (ActivityNotFoundException a) {
+                Toast.makeText(Listening.this, "Your device doesn't support Speech Recognition", Toast.LENGTH_SHORT).show();
+            }
         }
+
+
+        private void speak(String text) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+            } else {
+                tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        }
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100){
+            if (resultCode == RESULT_OK && null != data) {
+                ArrayList<String> res = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                String inSpeech = res.get(0);
+                recognition(inSpeech);
+            }
+        }
+    }
+
+    private void recognition(String text){
+        Log.e("Speech",""+text);
     }
 
     @Override
@@ -58,10 +102,11 @@ public class Listening extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listening);
-        settingsButton = (ImageView)findViewById(R.id.settings_button);
+        settingsButton = (ImageView) findViewById(R.id.settings_button);
         settingsButton.setOnClickListener(this);
-        startTime= SystemClock.uptimeMillis();
-        wait.postDelayed(waitTime,0);
+        startTime = SystemClock.uptimeMillis();
+        wait.postDelayed(waitTime, 0);
+        findViewById(R.id.micButton).setOnClickListener(this);
     }
 
     //Ian: Settings button, wait timer on answer, CheckAnswer (need to fix that logic)
@@ -69,6 +114,10 @@ public class Listening extends Activity implements View.OnClickListener {
         if (v.getId()==R.id.settings_button){
             startActivity(new Intent(Listening.this,SettingsActivity.class));
         }
+        else if (v.getId()==R.id.micButton){
+            listen();
+        }
+
     }
 
     private Runnable waitTime = new Runnable(){
@@ -87,4 +136,5 @@ public class Listening extends Activity implements View.OnClickListener {
         if(correct) startActivity(new Intent(Listening.this,AnswerCorrect.class));
         else startActivity(new Intent(Listening.this,AnswerIncorrect.class));
     }
+
 }
