e8108075e94de1ae0663996ebdf4bc73bdb8b8d4
Paladinv82 <autopaladinone@gmail.com>
2017-02-04 10:09:46 -0800

    create all activities (except Repeat/Swap)

--- app/src/main/java/com/ac/csun/team3/AnswerIncorrect.java
+++ app/src/main/java/com/ac/csun/team3/AnswerIncorrect.java
@@ -3,11 +3,11 @@ package com.ac.csun.team3;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class AnswerIncorrect extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_answer_incorrect);
     }
 }
