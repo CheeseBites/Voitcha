20736c255fa68f86818b739bf0346964733292be
George <gnasour@gmail.com>
2017-02-04 12:29:23 -0800

    XML changes and updated images

--- app/src/main/java/com/ac/csun/team3/LaunchActivity.java
+++ app/src/main/java/com/ac/csun/team3/LaunchActivity.java
@@ -1,9 +1,10 @@
 package com.ac.csun.team3;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.content.Intent;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
