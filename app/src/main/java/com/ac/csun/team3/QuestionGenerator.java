73e46840ef11c909324b7cb46bcfe831e1831fde
paladinv82 <autopaladinone@gmail.com>
2017-02-05 01:36:25 -0800

    Javadocs and comments; also resized Mic/Repeat/Skip button icons by 2.5-3x. Contains exactly 1 code Warning, which can be ignored because it's a 'deprecated method' warning on tts.speak, and we've already covered it.

--- app/src/main/java/com/ac/csun/team3/QuestionGenerator.java
+++ app/src/main/java/com/ac/csun/team3/QuestionGenerator.java
@@ -8,20 +8,20 @@ import java.util.Random;
  * Constructor generates a single question, to be referenced by .question and .answer fields
  * New questions are called by constructing new instances
  */
-
-public class QuestionGenerator {
+class QuestionGenerator {
     private Random random = new Random();
     private int n = random.nextInt(10) + 1;
     private int m = random.nextInt(10) + 1;
     int answer;
 
+    //"What is (number) plus (number)?"
     String question;
-    String q_part_1 = "What is ";
-    String q_part_2 = "plus ";
-    String q_part_3 = "?";
+    private String q_part_1 = "What is ";
+    private String q_part_2 = "plus ";
+    private String q_part_3 = "?";
 
-    QuestionGenerator() {
-        this.question = q_part_1 + n + q_part_2 + m + q_part_3; //TODO: Add additional types of questions
+    QuestionGenerator() { //TODO: Add additional types of questions
+        this.question = q_part_1 + n + q_part_2 + m + q_part_3;
         this.answer = n + m;
     }
 
